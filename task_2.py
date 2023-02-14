179eb47a119dd076b9171cc11e7c958d19fdd33b
Pro100-Adm <kirow.alesha@yandex.ru>
2023-02-14 17:04:32 +0300

    Seminar 11.02.2023 homework

--- task_2.py
+++ task_2.py
@@ -1,12 +1,13 @@
-def recursive_is_even(num, even = 0, odd = 0):
+def recursive_is_even(num, even=0, odd=0):
     if num == 0:
         return f"Quantity of even and odd numbers: {even, odd}"
     else:
         last = num % 10
-        if last%2 == 0:
+        if last % 2 == 0:
             even += 1
         else:
             odd += 1
-        return recursive_is_even(num//10, even, odd)
+        return recursive_is_even(num // 10, even, odd)
+
 
 print(recursive_is_even(int(input("Enter number: "))))
--- task_4.py
+++ task_4.py
@@ -1,8 +1,7 @@
 def sequency_summ(n):
-    if n==1:
-        return (-1)**(n+1)/(2**(n-1))
-    return (-1)**(n+1)/(2**(n-1)) + sequency_summ(n-1)
+    if n == 1:
+        return (-1) ** (n + 1) / (2 ** (n - 1))
+    return (-1) ** (n + 1) / (2 ** (n - 1)) + sequency_summ(n - 1)
+
 
 print(sequency_summ(int(input("Enter elements quantity: "))))
-
-
